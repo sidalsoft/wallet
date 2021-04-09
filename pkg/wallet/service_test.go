e18660a3444e28ada38855889704d8532ea3d720
Saidali Saburov <ssaburov@velox.biz>
2021-04-09 16:44:13 +0500

    -Z#1

--- pkg/wallet/service_test.go
+++ pkg/wallet/service_test.go
@@ -13,10 +13,14 @@ func TestService_FindAccountByID(t *testing.T) {
 	_, _ = srv.RegisterAccount("992928563352")
 	_, _ = srv.RegisterAccount("992928125354")
 
-	_, err := srv.FindAccountByID(2)
-	if err != nil {
-		if err != ErrAccountNotFound {
-			t.Errorf("Invalid result, expected : %v, actual %v", err, ErrAccountNotFound)
+	for i := 0; i < 5; i++ {
+		acc, err := srv.FindAccountByID(2)
+		if err != nil {
+			if err != ErrAccountNotFound {
+				t.Errorf("Invalid result, expected : %v, actual %v", err, ErrAccountNotFound)
+			}
+		} else if acc == nil {
+			t.Errorf("Invalid result, expected : %v, actual %v", "Account", acc)
 		}
 	}
 }
